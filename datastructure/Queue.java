d4bf00266dcdf8633af8e498a41d183062aeaa89
Shubham Raj <shubhamaryan805@gmail.com>
2022-08-03 22:05:56 +0530

    Added : Queue Enque

---
+++ datastructure/Queue.java
@@ -0,0 +1,20 @@
+package datastructure;
+
+public class Queue {
+    LinkedList linkedList = new LinkedList();
+
+    public void enque(Object data){
+        linkedList.addLast(data);
+        displayQueue();
+    }
+
+    public void deque(){
+        linkedList.deleteFirst();
+        displayQueue();
+    }
+
+    public void displayQueue(){
+        linkedList.display();
+    }
+
+}
